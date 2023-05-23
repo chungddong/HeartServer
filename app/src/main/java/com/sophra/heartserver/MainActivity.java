62ceb867c3676f56182cddd88be7de5cecb958fc
chungddong <chungman010918@gmail.com>
2023-05-23 23:18:13 +0900

    구글 스프레드시트를 db 삼아서 데이터 보내는 코드 작성

--- app/src/main/java/com/sophra/heartserver/MainActivity.java
+++ app/src/main/java/com/sophra/heartserver/MainActivity.java
@@ -5,12 +5,23 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Debug;
 import android.util.Log;
 import android.widget.TextView;
 
 import com.sophra.heartserver.databinding.ActivityMainBinding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Date;
 
 public class MainActivity extends Activity {
 
@@ -39,8 +50,82 @@ public class MainActivity extends Activity {
 
         sensorManager.registerListener(msensorEventListner,hearRate,SensorManager.SENSOR_DELAY_FASTEST);
 
+        //sendDataToGoogleSheet(100);
+
+    }
+
+    private void sendDataToGoogleSheet(float bpms) {
+        String scriptURL = "https://script.google.com/macros/s/AKfycby_KS2B6Wux73MycQl7DPVJuLtWFpHGKnoLMrd_eKTb1lrnRvIcaK0zuHesA5PF8LTRwQ/exec";
+        String name = "현재심박수";
+        float bpm = bpms;
+
+        new SendDataAsyncTask().execute(scriptURL, name, String.valueOf(bpm));
 
 
+    }
+
+    private static class SendDataAsyncTask extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            Log.d("heartserver", "실행");
+
+            String scriptUrl = params[0];
+            String name = params[1];
+            String age = params[2];
+
+            try {
+                URL url = new URL(scriptUrl);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoOutput(true);
+
+                String postData = "name=" + URLEncoder.encode(name, "UTF-8") +
+                        "&age=" + URLEncoder.encode(age, "UTF-8");
+
+                // 데이터 전송
+                OutputStream outputStream = connection.getOutputStream();
+                outputStream.write(postData.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+                // 응답 받기
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    response.append(line);
+                }
+                bufferedReader.close();
+                inputStream.close();
+
+
+                Log.d("heartserver", "response : " + response);
+                return response.toString();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            // 응답 처리
+            if (s != null) {
+                // 성공적으로 데이터를 저장한 경우
+                Log.d("heartserver", "onPostExecute: success");
+                // TODO: 처리 코드 작성
+            } else {
+                // 데이터 저장 실패한 경우
+                Log.d("heartserver", "onPostExecute: fail");
+                // TODO: 에러 처리 코드 작성
+            }
+        }
     }
 
     @Override
@@ -61,9 +146,13 @@ public class MainActivity extends Activity {
             text_bpm.setText(String.valueOf(sensorEvent.values[0]));
             if(sensorEvent.sensor.getType() == Sensor.TYPE_HEART_RATE)
             {
+                Log.d("heartserver", "센서값 변경");
                 String msg = "" + sensorEvent.values[0];
 
                 text_bpm.setText(String.valueOf(sensorEvent.values[0]));
+
+                Date date = new Date(System.currentTimeMillis());
+                sendDataToGoogleSheet(sensorEvent.values[0]);
             }
         }
 
